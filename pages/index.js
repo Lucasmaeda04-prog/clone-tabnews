dbd735d03a760a74f8cc2f81e79a61317f4bc533
Lucas Maeda <lucasmaedatrabalhos@gmail.com>
2026-03-09 23:20:39 -0300

    adiciona scripts de formatter

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home() { 
-    return <h1>Test 4: frase modificada para testes!</h1>
+function Home() {
+  return <h1>Test 4: frase modificada para testes!</h1>;
 }
 
-export default Home; 
+export default Home;
